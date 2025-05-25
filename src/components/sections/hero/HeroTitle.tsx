c8fb121e0d2ece01a0ca8f998c1fb1dae7fa0375
daveyhert <daveyhert@gmail.com>
2025-05-25 23:56:08 +0100

    feat: enhance HeroTitle component with animated text and dynamic dimensions

--- src/components/sections/hero/HeroTitle.tsx
+++ src/components/sections/hero/HeroTitle.tsx
@@ -1,12 +1,38 @@
-import SpotlightGlass from "../../ui/spotlight-glass/SpotlightGlass";
+import { useRef } from "react";
+import { motion } from "framer-motion";
+import SpotlightGlass from "@components/ui/spotlight-glass/SpotlightGlass";
+import { AnimatedWord } from "@components/ui/animated-word/AnimatedWord";
+import { useElementDimension } from "@hooks/useElementDimension";
 
 export default function HeroTitle() {
+  const wordRef = useRef<HTMLElement>(null);
+  const { width, height } = useElementDimension(wordRef);
+
   return (
     <div className='hero-title'>
-      {/* <h1>Meet the team of designers, and engineers behind the pixels.</h1> */}
       <h1>
-        Meet our team of innovators <span className='stylish-em-dash'>—</span>{" "}
-        the faces <SpotlightGlass>behind the pixels</SpotlightGlass>
+        Meet our team of{" "}
+        <motion.span
+          className='word-squeeze'
+          ref={wordRef}
+          animate={{ letterSpacing: ["-2px", "-6px", "-2px"] }}
+          transition={{ duration: 0.8, times: [0, 0.4, 0.8], delay: 2 }}
+        >
+          {Array.from({ length: 4 }).map((_, i) => (
+            <span className='dot' key={i} />
+          ))}
+          innovators
+          <span className='stylish-em-comma'>,</span>
+          <div className='word-width'>
+            {width} x {height}
+          </div>
+        </motion.span>{" "}
+        the{" "}
+        <div className='animated-word'>
+          <AnimatedWord />
+          <span>faces</span>
+        </div>{" "}
+        <SpotlightGlass>behind the pixels.</SpotlightGlass>
       </h1>
     </div>
   );
