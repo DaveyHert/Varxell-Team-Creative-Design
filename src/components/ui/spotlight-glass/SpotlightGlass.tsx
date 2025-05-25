2cccfd3e0396e3e9b8e553fefd53f354f66834ac
daveyhert <daveyhert@gmail.com>
2025-05-25 23:50:35 +0100

    fix: update SpotlightGlass to conditionally render motion span based on parent width

--- src/components/ui/spotlight-glass/SpotlightGlass.tsx
+++ src/components/ui/spotlight-glass/SpotlightGlass.tsx
@@ -13,7 +13,7 @@ const SpotlightGlass = ({ children }: SpotlightGlassProp) => {
   const updateSlideWidth = () => {
     if (ref.current) {
       const width = ref.current.offsetWidth;
-      const glassSize = 63; // should match CSS
+      const glassSize = 63; //  match CSS
       setParentWidth(width - glassSize);
     }
   };
@@ -29,25 +29,27 @@ const SpotlightGlass = ({ children }: SpotlightGlassProp) => {
 
   return (
     <div className='spotlight-words-wrapper' ref={ref}>
-      <motion.span
-        className='spotlight-glass'
-        animate={{
-          x: [
-            0,
-            parentWidth * 0.5,
-            parentWidth * 0.5,
-            parentWidth,
-            parentWidth * 0.77,
-          ],
-        }}
-        transition={{
-          duration: 6,
-          ease: "easeInOut",
-          times: [0, 0.24, 0.26, 0.5, 0.7],
-          delay: 0.3,
-          repeat: 0, // no repetition
-        }}
-      />
+      {parentWidth > 0 && (
+        <motion.span
+          className='spotlight-glass'
+          animate={{
+            x: [
+              0,
+              parentWidth * 0.5,
+              parentWidth * 0.5,
+              parentWidth,
+              parentWidth * 0.77,
+            ],
+          }}
+          transition={{
+            duration: 6,
+            ease: "easeInOut",
+            times: [0, 0.24, 0.26, 0.5, 0.7],
+            delay: 3,
+            repeat: 0,
+          }}
+        />
+      )}
       {children}
     </div>
   );
