847b0020fcec1d15f7783a86cb42cb1de884a89c
daveyhert <daveyhert@gmail.com>
2025-05-26 04:41:06 +0100

    fix: update FooterSection import and remove unused SvgHoverReveal component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,18 @@
 import "./App.css";
-import CareersSection from "./components/sections/careers/CareersSection";
-import HeroSection from "./components/sections/hero/HeroSection";
-import ResourcesSection from "./components/sections/resource/ResourcesSection";
-import TeamSection from "./components/sections/team/TeamSection";
-import SvgHoverReveal from "@components/sections/footer/Footer";
+import CareersSection from "@components/sections/careers/CareersSection";
+import HeroSection from "@components/sections/hero/HeroSection";
+import ResourcesSection from "@components/sections/resource/ResourcesSection";
+import TeamSection from "@components/sections/team/TeamSection";
+import FooterSection from "@components/sections/footer/FooterSection";
 
 function App() {
   return (
-    <div className='app'>
+    <div className="app">
       <HeroSection />
       <ResourcesSection />
       <TeamSection />
       <CareersSection />
-      <SvgHoverReveal />
-      <div className='footer' style={{ height: "15vh" }}>
-        footer
-      </div>
+      <FooterSection />
     </div>
   );
 }
