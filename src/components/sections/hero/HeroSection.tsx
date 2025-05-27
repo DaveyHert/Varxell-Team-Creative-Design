f3e1f8dc8b1e6a8fbbf35e3a9649aeb2f7e86cbd
daveyhert <daveyhert@gmail.com>
2025-05-27 13:50:26 +0100

    Add LogoIcon component and integrate into HeroSection

--- src/components/sections/hero/HeroSection.tsx
+++ src/components/sections/hero/HeroSection.tsx
@@ -3,11 +3,13 @@ import HeroTitle from "./HeroTitle";
 import HeroSubtitle from "./HeroSubtitle";
 import Navbar from "@components/ui/Navbar";
 import NoiseFilter from "@assets/NoiseFilter";
+import LogoIcon from "@assets/LogoIcon";
 
 export default function HeroSection() {
   return (
-    <section className='hero-section' id='Home'>
-      <div className='hero-content'>
+    <section className="hero-section" id="Home">
+      <div className="hero-content">
+        <LogoIcon />
         <HeroTitle />
         <HeroSubtitle />
         <Navbar />
